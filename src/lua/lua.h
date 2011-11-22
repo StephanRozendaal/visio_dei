550f4997795c88b6d60a6c019c283ff6a95c300a
Stephan <stephan.rozendaal@gmail.com>
2011-11-22 10:40:09 +0100

    ik was wat files vergeten.

---
+++ src/lua/lua.h
@@ -0,0 +1,27 @@
+/*
+ * lua.h
+ *
+ *  Created on: Nov 21, 2011
+ *      Author: stephan
+ */
+
+#ifndef LUA_H_
+#define LUA_H_
+namespace lua {
+#include <lua.hpp>
+#include <luabind/luabind.hpp>
+
+class lua_loader {
+public:
+
+private:
+
+protected:
+
+};
+
+
+
+
+}
+#endif /* LUA_H_ */
---
+++ src/lua/lua_loader.cpp
@@ -0,0 +1,10 @@
+/*
+ * lua_loader.cpp
+ *
+ *  Created on: Nov 21, 2011
+ *      Author: stephan
+ */
+#include "lua.h"
+
+
+
