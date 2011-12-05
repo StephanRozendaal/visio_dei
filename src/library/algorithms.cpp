bed90418f40d85f1594188d85dd50fd8add8d132
Stephan <stephan.rozendaal@gmail.com>
2011-12-05 13:05:58 +0100

    Oep! er ging wat mis met copy-pasten in de findSquares methode

--- src/library/algorithms.cpp
+++ src/library/algorithms.cpp
@@ -43,6 +43,8 @@ void findSquares(const cv::Mat& image,
 		std::vector<std::vector<cv::Point> >& squares) {
 	using namespace std;
 	using namespace cv;
+
+	//waardes voor threshold levels
 	int thresh = 50, N = 11;
 	squares.clear();
 
@@ -69,99 +71,66 @@ void findSquares(const cv::Mat& image,
 				// dilate canny output to remove potential
 				// holes between edge segments
 				dilate(gray, gray, Mat(), Point(-1, -1));
-			} else
-				squares.clear();
+			} else {
+				// apply threshold if l!=0:
+				//     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+				gray = gray0 >= (l + 1) * 255 / N;
+			}
 
-			Mat pyr, timg, gray0(image.size(), CV_8U), gray;
+			// find contours and store them all as a list
+			findContours(gray, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
 
-			// down-scale and upscale the image to filter out the noise
-			pyrDown(image, pyr, Size(image.cols / 2, image.rows / 2));
-			pyrUp(pyr, timg, image.size());
-			vector<vector<Point> > contours;
+			vector<Point> approx;
 
-			// find squares in every color plane of the image
-			for (int c = 0; c < 3; c++) {
-				int ch[] = { c, 0 };
-				mixChannels(&timg, 1, &gray0, 1, ch, 1);
+			// test each contour
+			for (size_t i = 0; i < contours.size(); i++) {
+				// approximate contour with accuracy proportional
+				// to the contour perimeter
+				approxPolyDP(Mat(contours[i]), approx,
+						arcLength(Mat(contours[i]), true) * 0.02, true);
 
-				// try several threshold levels
-				for (int l = 0; l < N; l++) {
-					// hack: use Canny instead of zero threshold level.
-					// Canny helps to catch squares with gradient shading
-					if (l == 0) {
-						// apply Canny. Take the upper threshold from slider
-						// and set the lower to 0 (which forces edges merging)
-						Canny(gray0, gray, 0, thresh, 5);
-						// dilate canny output to remove potential
-						// holes between edge segments
-						dilate(gray, gray, Mat(), Point(-1, -1));
-					} else {
+				// square contours should have 4 vertices after approximation
+				// relatively large area (to filter out noisy contours)
+				// and be convex.
+				// Note: absolute value of an area is used because
+				// area may be positive or negative - in accordance with the
+				// contour orientation
+				if (approx.size() == 4 && fabs(contourArea(Mat(approx))) > 1000
+						&& isContourConvex(Mat(approx))) {
+					double maxCosine = 0;
 
-						{
-							// apply threshold if l!=0:
-							//     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
-							gray = gray0 >= (l + 1) * 255 / N;
-						}
-
-						// find contours and store them all as a list
-						findContours(gray, contours, CV_RETR_LIST,
-								CV_CHAIN_APPROX_SIMPLE);
-
-						vector<Point> approx;
-
-						// test each contour
-						for (size_t i = 0; i < contours.size(); i++) {
-							// approximate contour with accuracy proportional
-							// to the contour perimeter
-							approxPolyDP(Mat(contours[i]), approx,
-									arcLength(Mat(contours[i]), true) * 0.02,
-									true);
-
-							// square contours should have 4 vertices after approximation
-							// relatively large area (to filter out noisy contours)
-							// and be convex.
-							// Note: absolute value of an area is used because
-							// area may be positive or negative - in accordance with the
-							// contour orientation
-							if (approx.size() == 4
-									&& fabs(contourArea(Mat(approx))) > 1000
-									&& isContourConvex(Mat(approx))) {
-								double maxCosine = 0;
-
-								for (int j = 2; j < 5; j++) {
-									// find the maximum cosine of the angle between joint edges
-									double cosine = fabs(
-											angle(approx[j % 4], approx[j - 2],
-													approx[j - 1]));
-									maxCosine = MAX(maxCosine, cosine);
-								}
-
-								// if cosines of all angles are small
-								// (all angles are ~90 degree) then write quandrange
-								// vertices to resultant sequence
-								if (maxCosine < 0.3)
-									squares.push_back(approx);
-							}
-						}
+					for (int j = 2; j < 5; j++) {
+						// find the maximum cosine of the angle between joint edges
+						double cosine = fabs(
+								angle(approx[j % 4], approx[j - 2],
+										approx[j - 1]));
+						maxCosine = MAX(maxCosine, cosine);
 					}
+
+					// if cosines of all angles are small
+					// (all angles are ~90 degree) then write quandrange
+					// vertices to resultant sequence
+					if (maxCosine < 0.3)
+						squares.push_back(approx);
 				}
 			}
 		}
 	}
 }
-/**
- * tekent de rechthoeken op het image uit de parameters
- */
-void drawSquares(cv::Mat& image, std::vector<std::vector<cv::Point> >& squares){
-	using namespace cv;
-	for( size_t i = 0; i < squares.size(); i++ )
-		{
-			const Point* p = &squares[i][0];
-			int n = (int)squares[i].size();
-			polylines(image, &p, &n, 1, true, Scalar(0,255,0), 3, CV_AA);
-		}
+}
 
-		resize(image, image, Size(), 3.0, 3.0, INTER_LINEAR);
+/**
+ * tekent de rechthoeken op het image uit de parameter
+ */
+void drawSquares(cv::Mat& image,
+	std::vector<std::vector<cv::Point> >& squares) {
+using namespace cv;
+for (size_t i = 0; i < squares.size(); i++) {
+	const Point* p = &squares[i][0];
+	int n = (int) squares[i].size();
+	polylines(image, &p, &n, 1, true, Scalar(0, 255, 0), 3, CV_AA);
+}
+resize(image, image, Size(), 3.0, 3.0, INTER_LINEAR);
 }
 }
-}
+
--- src/library/algorithms.h
+++ src/library/algorithms.h
@@ -3,24 +3,18 @@
  *
  *  Created on: Nov 25, 2011
  *      Author: stephan
- *
- * In deze file komen allemaal segmentatie functies.
- * 	- segmentatie door thresholding
- * 	- kleur segmentatie
- * 	- region growing
- * 	- segmentatie van regiogeometrische structuur
  */
 
 #ifndef ALGORITHMS_H_
 #define ALGORITHMS_H_
 
 namespace alg {
-image_resource change_color_space(image_resource input, int code);
+image_resource change_color_space(const image_resource, int);
 namespace segmentation {
-image_resource threshold(image_resource input, double thresh, double max, int type);
+image_resource threshold(const image_resource, double, double, int);
 }
 namespace detection {
-double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0);
+double angle(cv::Point, cv::Point, cv::Point);
 void findSquares(const cv::Mat&, std::vector<std::vector<cv::Point> >&);
 void drawSquares(cv::Mat&, std::vector<std::vector<cv::Point> >&);
 }
