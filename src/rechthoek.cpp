461d6649228873bcb57ac46e071e12c3c0db22e3
Stephan <stephan.rozendaal@gmail.com>
2012-01-04 16:24:14 +0100

    vind_rechthoek programma fixed

--- src/rechthoek.cpp
+++ src/rechthoek.cpp
@@ -3,15 +3,15 @@
  *
  *       Filename:  rechthoek.cpp
  *
- *    Description:  
+ *    Description:
  *
  *        Version:  1.0
  *        Created:  12/22/2011 11:25:28 AM
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
- *        Company:  
+ *         Author:  YOUR NAME (),
+ *        Company:
  *
  * =====================================================================================
  */
@@ -21,13 +21,14 @@ using namespace std;
 int main() {
 
   video_resource video(0);
+  image_resource detection_img;
   window2D window;
 
   vector<vector< cv::Point> > buffer;
   while(1) {
-    alg::detection::findSquares(video, buffer);
-    alg::detection::drawSquares(video,buffer);
-    window.addToDraw(video);
+    buffer = alg::detection::findSquares(video);
+    detection_img = alg::detection::drawSquares(video,buffer);
+    window.addToDraw(detection_img);
     window.Display();
   }
   return 0;
